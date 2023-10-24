e32b8450ef11187094c5edbc19c43807e5ef4bf6
Michael Sproul <michael@sigmaprime.io>
2023-10-25 09:05:20 +1100

    Use triomphe feature for benches

--- benches/archery_shared_pointer_arc.rs
+++ benches/archery_shared_pointer_arc.rs
@@ -11,6 +11,9 @@ use std::ops::Deref;
 use criterion::{criterion_group, criterion_main, Criterion};
 use std::hint::black_box;
 
+#[cfg(feature = "triomphe")]
+use archery::ArcTK as ArcK;
+
 fn archery_shared_pointer_arc_deref(c: &mut Criterion) {
     let limit = 200_000;
 
@@ -41,41 +44,5 @@ fn archery_shared_pointer_arc_clone(c: &mut Criterion) {
     });
 }
 
-fn archery_shared_pointer_arct_deref(c: &mut Criterion) {
-    let limit = 200_000;
-
-    c.bench_function("archery shared pointer arct deref", move |b| {
-        b.iter(|| {
-            let rc: SharedPointer<_, ArcTK> = SharedPointer::new(42);
-
-            for _ in 0..limit {
-                black_box(rc.deref());
-            }
-
-            rc
-        })
-    });
-}
-
-fn archery_shared_pointer_arct_clone(c: &mut Criterion) {
-    let limit = 100_000;
-
-    c.bench_function("archery shared pointer arct clone and drop", move |b| {
-        b.iter_with_setup(
-            || Vec::with_capacity(limit),
-            |mut vec| {
-                vec.resize(limit, SharedPointer::<_, ArcTK>::new(42));
-                vec
-            },
-        )
-    });
-}
-
-criterion_group!(
-    benches,
-    archery_shared_pointer_arc_deref,
-    archery_shared_pointer_arc_clone,
-    archery_shared_pointer_arct_deref,
-    archery_shared_pointer_arct_clone
-);
+criterion_group!(benches, archery_shared_pointer_arc_deref, archery_shared_pointer_arc_clone,);
 criterion_main!(benches);
